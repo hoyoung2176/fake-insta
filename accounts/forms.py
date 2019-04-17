6f98d4e34b2b1ac68bff3c9d921db4e61b9b0c9e
hoyoung2176 <hoyoung2176@gamil.com>
2019-04-17 04:12:12 +0000

    08_Auth update delete && password update

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,12 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
 
 class UserCustomCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = ['username', 'email',]
-        
+    
+class UserCustomChangeForm(UserChangeForm):
+    class Meta:
+        model = get_user_model()
+        fields = ['email', 'first_name', 'last_name',]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login as auth_login
-from django.contrib.auth.forms import AuthenticationForm
-from .forms import UserCustomCreationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from .forms import UserCustomCreationForm, UserCustomChangeForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
 
 
 # Create your views here.
@@ -16,7 +18,8 @@ def signup(request):
     if request.method == 'POST':
         form = UserCustomCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            auth_login(request, user)
             return redirect('posts:list')
     else:
         form = UserCustomCreationForm()
@@ -52,3 +55,45 @@ def people(request, username):
     people = get_object_or_404(get_user_model(), username=username)
     context = {'people':people,}
     return render(request, 'accounts/people.html', context)
+    
+
+# # @require_POST
+@login_required 
+def update(request):
+    if request.method == 'POST':
+        user_change_form = UserCustomChangeForm(request.POST, instance = request.user)
+        if user_change_form.is_valid():
+            user_change_form.save()
+            return redirect('people', request.user.username)
+        pass
+    else:
+        user_change_form = UserCustomChangeForm(instance = request.user)
+        
+    context={
+        'user_change_form':user_change_form,
+    }
+    return render(request, 'accounts/update.html', context)
+
+@login_required 
+def delete(request):
+    if request.method == 'POST':
+        user = request.user
+        user.delete()
+    return redirect('posts:list')
+
+@login_required 
+def password(request):
+    if request.method == "POST":
+        password_change_form = PasswordChangeForm(user=request.user, data=request.POST)
+        if password_change_form.is_valid():
+            user = password_change_form.save()
+            update_session_auth_hash(request, user)
+            return redirect('posts:list')
+            
+    else:
+        password_change_form = PasswordChangeForm(request.user)
+    context = {
+        'password_change_form':password_change_form,
+    }
+    return render(request, 'accounts/password.html', context)
+    
